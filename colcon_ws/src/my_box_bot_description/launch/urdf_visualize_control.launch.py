bf7500445c8099a17860ba2d30977b7ff43c3aaf
kumar-sanjeeev <kumar.san96@gmail.com>
2023-10-16 13:34:35 +0200

    add: robot control through gazebo plugin and ros2_control

---
+++ colcon_ws/src/my_box_bot_description/launch/urdf_visualize_control.launch.py
@@ -0,0 +1,45 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    # Input Data Files
+    urdf_file = "box_bot_physical_control.urdf"
+    package_description = "my_box_bot_description"
+
+    print("Fetching URDF ====>")
+    robot_description_path = os.path.join(get_package_share_directory(package_name=package_description), "urdf", urdf_file)
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher_node",
+        output="screen",
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_description_path])}],   
+    )
+
+    # RVIZ Configuration
+    rviz_file = "urdf_vis_scale.rviz"
+    rviz_config_dir = os.path.join(get_package_share_directory(package_name=package_description), "rviz", rviz_file)
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        name="rviz_node",
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir])
+    
+
+    # Create and return launch description object
+    return LaunchDescription(
+        [
+            robot_state_publisher_node,
+            rviz_node
+        ]
+    )
